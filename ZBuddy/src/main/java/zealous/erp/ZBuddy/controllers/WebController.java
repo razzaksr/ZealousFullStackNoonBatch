d10cad09d7804da538650a22a8db49f159770617
razzaksr <razzaksr@gmail.com>
2021-07-14 15:07:01 +0530

    enrollemnt 1

--- ZBuddy/src/main/java/zealous/erp/ZBuddy/controllers/WebController.java
+++ ZBuddy/src/main/java/zealous/erp/ZBuddy/controllers/WebController.java
@@ -15,19 +15,19 @@ public class WebController
 	@Autowired
 	StudentsService service;
 	
-	@RequestMapping("/add")
-	public String enroll(Model model)
+	@RequestMapping("/")
+	public String ins(Model model)
 	{
 		Student student=new Student();
 		model.addAttribute("object", student);
-		return "enroll";
+		return "new";
 	}
 	
 	@RequestMapping(value="/adding",method=RequestMethod.POST)
-	public String enrolling(Model model,Student object)
+	public String insertings(Model model,Student object)
 	{
 		String out=service.insertion(object);
 		model.addAttribute("msg", out);
-		return "enroll";
+		return "new";
 	}
 }
