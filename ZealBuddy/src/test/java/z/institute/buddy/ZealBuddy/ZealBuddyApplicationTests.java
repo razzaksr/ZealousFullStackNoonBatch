ac12523037208a13c692e63157f6748bd91b7a75
razzaksr <razzaksr@gmail.com>
2021-08-08 11:02:23 +0530

    success test

--- ZealBuddy/src/test/java/z/institute/buddy/ZealBuddy/ZealBuddyApplicationTests.java
+++ ZealBuddy/src/test/java/z/institute/buddy/ZealBuddy/ZealBuddyApplicationTests.java
@@ -72,7 +72,7 @@ class ZealBuddyApplicationTests {
 				new Student(45,"Sabarinathan S","Ponnammapet, Salem","Excel",8765678987678L,"sabariragu@gmail.com","java full stack",25000,10000,new Date(2021,01,20),new Date(2021,01,20),new Date(2021,01,25),null,true,false),
 				new Student(11,"Meharaj S","Ponnammapet, Salem","Excel",8765678987678L,"meharaj@gmail.com","python",8000,1000,new Date(2021,01,20),new Date(2021,01,20),new Date(2021,01,25),null,true,false)).collect(Collectors.toList()));
 		
-		assertTrue(service.viewAll().size()>5);
+		assertTrue(service.viewAll().size()>2);
 		assertSame("Ponnammapet, Salem", service.viewAll().get(2).getAddress());
 	}
 	
@@ -82,7 +82,7 @@ class ZealBuddyApplicationTests {
 		when(repository.findAllByName("Razak")).thenReturn(Stream.of(
 				new Student(12,"Razak Mohamed S","Salem","Muthayammal",9876545678L,"razzaksr@gmail.com","java full stack",25000,10000,new Date(2021,01,20),new Date(2021,01,20),new Date(2021,01,25),null,true,false),
 				new Student(45,"Shahul Razak","Chennai","SRM",9876545678L,"razzaksr@gmail.com","python",25000,10000,new Date(2021,01,20),new Date(2021,01,20),new Date(2021,01,25),null,true,false)).collect(Collectors.toList()));
-		assertEquals(12,service.viewAllViaName("Razak").get(1).getNumber());
+		assertEquals(12,service.viewAllViaName("Razak").get(0).getNumber());
 		
 		when(repository.findAllByCourse("java")).thenReturn(Stream.of(
 				new Student(12,"Razak Mohamed S","Salem","Muthayammal",9876545678L,"razzaksr@gmail.com","java full stack",25000,10000,new Date(2021,01,20),new Date(2021,01,20),new Date(2021,01,25),null,true,false),
